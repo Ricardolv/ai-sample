2ff52a147d263b415ea931bcd3c46bdb4f705aaa
Ricardo Viana <richardluizv@gmail.com>
2025-04-19 20:19:16 -0300

    add project helo-world spring ai open-ai

--- api-ai/src/main/java/com/richard/api_ai/infrastruture/resources/ChatResource.java
+++ api-ai/src/main/java/com/richard/api_ai/infrastruture/resources/ChatResource.java
@@ -1,4 +1,24 @@
 package com.richard.api_ai.infrastruture.resources;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChatResource {
+
+    private final ChatClient chatClient;
+
+    public ChatResource(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+
 }
